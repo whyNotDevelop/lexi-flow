8d0a3a967a1b765bd28d659af26959e0a43b1c48
whyNotDevelop <sicelothusi91@gmail.com>
2026-03-19 09:28:10 +0200

    Update models.py
    Add history app models/tables

--- lexiflow_backend/history/infrastructure/models.py
+++ lexiflow_backend/history/infrastructure/models.py
@@ -1,3 +1,16 @@
+import uuid
 from django.db import models
+from users.infrastructure.models import User
+from words.infrastructure.models import Word
 
-# Create your models here.
+class LookupHistory(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='lookup_history')
+    word = models.ForeignKey(Word, on_delete=models.CASCADE)
+    looked_up_at = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    class Meta:
+        ordering = ['-looked_up_at']
+
+    def __str__(self):
+        return f"{self.user.email} - {self.word.text} at {self.looked_up_at}"
