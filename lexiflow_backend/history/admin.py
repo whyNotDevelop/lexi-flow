09fdfcd88a2854c195c7a4e9c8c663dc14a32149
whyNotDevelop <sicelothusi91@gmail.com>
2026-03-19 09:44:38 +0200

    Update history/admin.py

--- lexiflow_backend/history/admin.py
+++ lexiflow_backend/history/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .infrastructure.models import LookupHistory
 
-# Register your models here.
+@admin.register(LookupHistory)
+class LookupHistoryAdmin(admin.ModelAdmin):
+    list_display = ('user', 'word', 'looked_up_at')
+    list_filter = ('looked_up_at',)
+    search_fields = ('user__email', 'word__text')
+    readonly_fields = ('id', 'looked_up_at')
+    raw_id_fields = ('user', 'word')
+    date_hierarchy = 'looked_up_at'
