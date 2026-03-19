6fb403d5257ce5ee5d34e6445d81360f9c1e09a8
whyNotDevelop <sicelothusi91@gmail.com>
2026-03-19 09:45:55 +0200

    Update analytics/admin

--- lexiflow_backend/analytics/admin.py
+++ lexiflow_backend/analytics/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .infrastructure.models import ReadingSession
 
-# Register your models here.
+@admin.register(ReadingSession)
+class ReadingSessionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'book_title', 'started_at', 'duration_seconds', 'words_looked_up')
+    list_filter = ('started_at',)
+    search_fields = ('user__email', 'book_title', 'book_id')
+    readonly_fields = ('id', 'started_at')
+    raw_id_fields = ('user',)
+    date_hierarchy = 'started_at'
