8e919d9701a7b12cabcce7b3be62c982eb42e713
whyNotDevelop <sicelothusi91@gmail.com>
2026-04-04 22:36:58 +0200

    Create production settings

---
+++ lexiflow_backend/lexiflow_backend/settings/prod.py
@@ -0,0 +1,25 @@
+"""
+Production settings for LexiFlow backend.
+These settings assume HTTPS termination is handled by a reverse proxy.
+"""
+
+from .settings import *
+
+DEBUG = False
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
+CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS', default=[])
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# SECURE_SSL_REDIRECT = True  # conditional
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+X_FRAME_OPTIONS = 'DENY'
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # noqa: F405
+
+# Override logging to ensure stdout only (already configured in settings.py, but we can leave as is)
